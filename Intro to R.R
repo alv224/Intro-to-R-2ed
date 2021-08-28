baf54c42e24ef479e219c9da89cbbae32504d086
Amel <alv224@lehigh.edu>
2021-08-28 15:35:43 -0400

    Added more student info

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for Amel Vernon
+# Copy for Amel Vernon, 2021-22 School Year
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
